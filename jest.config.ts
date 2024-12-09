1f55162a8d83bab6669d2c3918db3c8270c24689
Khaled Yacout <khaled.yacout@gmail.com>
2024-12-10 01:30:37 +0200

    add test coverage and remove coderabbit config file

--- jest.config.ts
+++ jest.config.ts
@@ -10,17 +10,21 @@ const config: Config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
   moduleDirectories: ['node_modules', '<rootDir>'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  transformIgnorePatterns: [
-    '/node_modules/(?!(msw|@mswjs)/)',
-  ],
-  testMatch: [
-    '**/__tests__/**/*.test.[jt]s?(x)',
-  ],
+  transformIgnorePatterns: ['/node_modules/(?!(msw|@mswjs)/)'],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
 };
 
 export default createJestConfig(config);
